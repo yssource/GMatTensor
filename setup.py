35da576f1378ba78690947a66fb6428072ea4071
Tom de Geus <tom@geus.me>
2021-04-04 16:23:44 +0200

    [Python] Reducing deps

--- setup.py
+++ setup.py
@@ -50,6 +50,6 @@ setup(
     author_email = 'tom@geus.me',
     url = 'https://github.com/tdegeus/GMatTensor',
     ext_modules = ext_modules,
-    install_requires = ['pybind11', 'pyxtensor'],
+    setup_requires = ['pybind11', 'pyxtensor'],
     cmdclass = {'build_ext': build},
     zip_safe = False)
