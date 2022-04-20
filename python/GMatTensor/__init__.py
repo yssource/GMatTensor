f0a1ed9a34035b7866c22ac1200e4031d1209591
Tom de Geus <tom@geus.me>
2022-04-20 10:36:51 +0200

    pre-commit

--- python/GMatTensor/__init__.py
+++ python/GMatTensor/__init__.py
@@ -1,3 +1,3 @@
-from ._GMatTensor import *  # noqa: F401,F403
 from . import Cartesian2d  # noqa: F401,F403
 from . import Cartesian3d  # noqa: F401,F403
+from ._GMatTensor import *  # noqa: F401,F403
