0ee422f79c1e01eca2739a82d95e9b727f0a7825
Tom de Geus <tdegeus@users.noreply.github.com>
2022-04-21 19:04:00 +0200

    [BREAKING CHANGE] Moving "allocate" to public namespace (#42)

--- include/GMatTensor/config.h
+++ include/GMatTensor/config.h
@@ -55,6 +55,57 @@ The advantage is that:
 Tensor products / operations.
 */
 namespace GMatTensor {
-}
+
+/**
+Helper to allocate 'output' which is of the same type of some 'input', but of a different rank.
+For example:
+\code
+// input: array of 2nd order tensors, shape = [..., ndim, ndim].
+// output: array of scalars, shape = [...]
+template <class T>
+auto return_scalar_array(const T& A2) ->
+    typename GMatTensor::allocate<xt::get_rank<T>::value - 2, T>::type;
+\endcode
+
+\tparam RANK Rank of the output.
+\tparam T Type of the 'input'
+*/
+template <size_t RANK, class T>
+struct allocate {
+};
+
+/**
+\cond
+*/
+template <size_t RANK, class EC, size_t N, xt::layout_type L, class Tag>
+struct allocate<RANK, xt::xtensor<EC, N, L, Tag>> {
+    using type = typename xt::xtensor<EC, RANK, L, Tag>;
+};
+
+#ifdef XTENSOR_FIXED_HPP
+template <size_t RANK, class EC, class S, xt::layout_type L>
+struct allocate<RANK, xt::xtensor_fixed<EC, S, L>> {
+    using type = typename xt::xtensor<EC, RANK, L>;
+};
+#endif
+
+#ifdef XTENSOR_FIXED_HPP
+template <size_t RANK, class EC, class S, xt::layout_type L, bool SH, class Tag>
+struct allocate<RANK, xt::xfixed_container<EC, S, L, SH, Tag>> {
+    using type = typename xt::xtensor<EC, RANK, L, Tag>;
+};
+#endif
+
+#ifdef PY_TENSOR_HPP
+template <size_t RANK, class EC, size_t N, xt::layout_type L>
+struct allocate<RANK, xt::pytensor<EC, N, L>> {
+    using type = typename xt::pytensor<EC, RANK, L>;
+};
+#endif
+/**
+\endcond
+*/
+
+} // namespace GMatTensor
 
 #endif
